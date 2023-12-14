d38ee947fa872f477e038315618e9df693fc3815
Sravani Ramineni <musunurisravani34@gmail.com>
2023-12-14 10:33:47 +0100

    Update gmail.cy.js

--- gmail.cy.js
+++ gmail.cy.js
@@ -2,5 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://gmail.com')
     cy.get('#identifierNext > .VfPpkd-dgl2Hf-ppHlrf-sM5MNb > .VfPpkd-LgbsSe > .VfPpkd-vQzf8d').click()
+    this is my cypress code
   })
 })
